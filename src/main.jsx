90b4886e71124594ac66ce4a8f2f3b60b4846298
BubaiMahanta <chotanmahanta5365@gmail.com>
2025-05-01 14:37:53 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+// src/main.jsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { UserProvider } from './context/UserContext.jsx';
+import { ChatProvider } from './context/ChatContext.jsx';
+
+export const server = "http://localhost:8000"; // Replace with your backend URL
+// export const server = "https://tarunaichatserver.onrender.com"; // Replace with your backend URL
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <UserProvider>
+      <ChatProvider>
+        <App />
+      </ChatProvider>
+    </UserProvider>
+  </StrictMode>
+);
